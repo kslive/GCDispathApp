d7fe98f852615826ba4dde54e9765515910a1813
ksllive <zhenya-kiselev-97@mail.ru>
2020-08-17 08:36:54 +0300

    Prepare for GCD.

--- GCDispathApp/SecondViewController.swift
+++ GCDispathApp/SecondViewController.swift
@@ -10,7 +10,39 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    private var imageURL: URL?
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            
+            activityIndicator.stopAnimating()
+            activityIndicator.isHidden = true
+            
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        fetchImage()
+    }
+    
+    private func fetchImage() {
+        
+        imageURL = URL(string: "https://upload.wikimedia.org/wikipedia/commons/0/07/Huge_ball_at_Vilnius_center.jpg")
+        
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        guard let url = imageURL,
+            let imageData = try? Data(contentsOf: url) else { return }
+        self.image = UIImage(data: imageData)
     }
 }
